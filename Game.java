23d1b0b92097c3eb8d85d55d9d0d0a62e0d92f72
CSC171FullGPA <shuang48@u.rochester.edu>
2019-04-21 05:04:15 +0800

    extra cr

--- Game.java
+++ Game.java
@@ -15,6 +15,8 @@ public class Game extends JPanel implements KeyListener, ActionListener {
 	private double SPEED = 4, vx=1, vy=1;
 	private int padH = 10, padW = 140;
 	private int blockX = 300, blockY = 200;
+	private int block2X = 100, block2Y = 300;
+	private int block3X = 600, block3Y = 200;
 	private int bottomPadX;
 	private int inset = 10;
 	// ball
@@ -59,8 +61,14 @@ public class Game extends JPanel implements KeyListener, ActionListener {
 		g2d.drawString(scoreB, 10, height / 2);
 		
 		// Block1
-		Rectangle2D block = new Rectangle(blockX, blockY, 150, 150);
+		Rectangle2D block = new Rectangle(blockX, blockY, 75, 75);
 		g2d.fill(block);
+		
+		Rectangle2D block2 = new Rectangle(block2X, block2Y, 50, 50);
+		g2d.fill(block2);
+		
+		Rectangle2D block3 = new Rectangle(block3X, block3Y, 100, 100);
+		g2d.fill(block3);
 
 	}
 	double velX=3*Math.cos(angle), velY=2.3;
@@ -70,27 +78,71 @@ public class Game extends JPanel implements KeyListener, ActionListener {
 		double g=0.0098;
 		velY+=g;
 		
-		//block
+		//block1
 		if (ballY - blockY + 20 <= 3 && velY > 0 && ballY - blockY+20 >= 0) {
-			if (ballX + ballSize >= blockX && ballX <= blockX + 150) {
+			if (ballX + ballSize >= blockX && ballX <= blockX + 75) {
 				velY = -velY;
 			}
 		}
-		if (ballY - blockY - 150 <= 3 && velY < 0 && ballY - blockY - 150 >=0) {
-			if (ballX + ballSize >= blockX && ballX <= blockX + 150) {
+		if (ballY - blockY - 75 <= 3 && velY < 0 && ballY - blockY - 75 >=0) {
+			if (ballX + ballSize >= blockX && ballX <= blockX + 75) {
 				velY = -velY;
 			}
 		}
-		if (ballX - blockX - 150 <= 3 && ballX - blockX - 150 >=0) {
-			if (ballY + ballSize >= blockY && ballY <= blockY + 150) {
+		if (ballX - blockX - 75 <= 3 && ballX - blockX - 75 >=0) {
+			if (ballY + ballSize >= blockY && ballY <= blockY + 75) {
 				velX = -velX;
 			}
 		}
 		if (ballX - blockX +20 <= 3  && ballX - blockX +20 >=0) {
-			if (ballY + ballSize >= blockY && ballY <= blockY + 150) {
+			if (ballY + ballSize >= blockY && ballY <= blockY + 75) {
 				velX = -velX;
 			}
 		}
+		
+		//block2
+				if (ballY - block2Y + 20 <= 3 && velY > 0 && ballY - block2Y+20 >= 0) {
+					if (ballX + ballSize >= block2X && ballX <= block2X + 50) {
+						velY = -velY;
+					}
+				}
+				if (ballY - block2Y - 50 <= 3 && velY < 0 && ballY - block2Y - 50 >=0) {
+					if (ballX + ballSize >= block2X && ballX <= block2X + 50) {
+						velY = -velY;
+					}
+				}
+				if (ballX - block2X - 50 <= 3 && ballX - block2X - 50 >=0) {
+					if (ballY + ballSize >= block2Y && ballY <= block2Y + 50) {
+						velX = -velX;
+					}
+				}
+				if (ballX - block2X +20 <= 3  && ballX - block2X +20 >=0) {
+					if (ballY + ballSize >= block2Y && ballY <= block2Y + 50) {
+						velX = -velX;
+					}
+				}
+			
+				//block3
+				if (ballY - block3Y + 20 <= 3 && velY > 0 && ballY - block3Y+20 >= 0) {
+					if (ballX + ballSize >= block3X && ballX <= block3X + 100) {
+						velY = -velY;
+					}
+				}
+				if (ballY - block3Y - 100 <= 3 && velY < 0 && ballY - block3Y - 100 >=0) {
+					if (ballX + ballSize >= block3X && ballX <= block3X + 100) {
+						velY = -velY;
+					}
+				}
+				if (ballX - block3X - 100 <= 3 && ballX - block3X - 100 >=0) {
+					if (ballY + ballSize >= block3Y && ballY <= block3Y + 100) {
+						velX = -velX;
+					}
+				}
+				if (ballX - block3X +20 <= 3  && ballX - block3X +20 >=0) {
+					if (ballY + ballSize >= block3Y && ballY <= block3Y + 100) {
+						velX = -velX;
+					}
+				}
 		
 		// side walls
 		

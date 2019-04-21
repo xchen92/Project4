2b702eb2ee249f0dd45d2396393aa05063360c88
CSC171FullGPA <shuang48@u.rochester.edu>
2019-04-22 01:47:51 +0800

    完成

--- Game.java
+++ Game.java
@@ -4,18 +4,22 @@ import java.awt.geom.Ellipse2D;
 import java.awt.geom.Rectangle2D;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Random;
+
 import javax.swing.*;
 
 public class Game extends JPanel implements KeyListener, ActionListener {
 	private int height, width;
 	private Timer t = new Timer(5, this);
+	private Timer t2 = new Timer(1000, new TimerHandeler());
 	private boolean first;
 	private HashSet<String> keys = new HashSet<String>();
 	// pad
 	private double SPEED = 4, vx=1, vy=1;
 	private int padH = 10, padW = 140;
-	private int blockX = 300, blockY = 200;
-	private int block2X = 100, block2Y = 300;
+	Random rand = new Random();
+	private int blockX = 200, blockY = 200;
+	private int block2X = 400, block2Y = 300;
 	private int block3X = 600, block3Y = 200;
 	private int bottomPadX;
 	private int inset = 10;
@@ -23,8 +27,11 @@ public class Game extends JPanel implements KeyListener, ActionListener {
 	private double angle = 35*Math.PI/180;
 	private double ballX, ballY, ballSize = 20;
 	// score
-	private int scoreBottom;
+	private int scoreBottom, lev=1, countDown = 20;
 	double time=0;
+	int lives=3;
+	
+	JLabel L1 = new JLabel("Press left or right key to start the game!");
 	
 	public Game() {
 		addKeyListener(this);
@@ -32,9 +39,22 @@ public class Game extends JPanel implements KeyListener, ActionListener {
 		setFocusTraversalKeysEnabled(false);
 		first = true;
 		t.setInitialDelay(100);
-		t.start();
-	}
+		add(L1,BorderLayout.NORTH);
+		
 
+	}
+	protected class TimerHandeler implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+			countDown--;
+			repaint();
+			if(countDown==0){
+				t2.stop();
+
+			}
+		}
+	}
+	
+		
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
@@ -44,8 +64,10 @@ public class Game extends JPanel implements KeyListener, ActionListener {
 		// initial positioning
 		if (first) {
 			bottomPadX = width / 2 - padW / 2;
-			ballX = width / 2 - ballSize / 2;
-			ballY = height / 2 - ballSize / 2;
+			ballX = 20;
+			ballY = 20;
+			//ballX = width / 2 - ballSize / 2;
+			//ballY = height / 3 - ballSize / 2;
 			first = false;
 		}
 		// bottom pad
@@ -57,8 +79,21 @@ public class Game extends JPanel implements KeyListener, ActionListener {
 		g2d.fill(ball);
 
 		// scores
-		String scoreB = "Bottom: " + new Integer(scoreBottom).toString();
-		g2d.drawString(scoreB, 10, height / 2);
+		String score = "SCORE: " + new Integer(scoreBottom).toString();
+		g2d.drawString(score, 10, height / 2);
+		
+		// lives
+		String life = "Number of lives: " + new Integer(lives).toString();
+		g2d.drawString(life, 10, height / 2+15);
+		
+		// Level
+		String level = "LEVEL: " + new Integer(lev).toString();
+		g2d.drawString(level, 10, height / 25);
+		
+		// countDown
+		String count = "time remaining: " + new Integer(countDown).toString();
+		g2d.drawString(count, 630, 30);
+		
 		
 		// Block1
 		Rectangle2D block = new Rectangle(blockX, blockY, 75, 75);
@@ -69,15 +104,41 @@ public class Game extends JPanel implements KeyListener, ActionListener {
 		
 		Rectangle2D block3 = new Rectangle(block3X, block3Y, 100, 100);
 		g2d.fill(block3);
+		
+		// Game over
+		if(lives<=0) {
+			String over = "Game Over! Your final score is: " + new Integer(scoreBottom).toString();
+			g2d.drawString(over, width/2-100, height / 20);
+			String again = "Press left or right to start another game!";
+			g2d.drawString(again, width/2-100, height / 20+20);
+			velX=3*Math.cos(angle);
+			velY=0.7;
+			ballX = 20;
+			ballY = 20;
+			t.stop();
+			t2.stop();
+			countDown = 20;
+			lives = 3;
+			scoreBottom = 0;
+		}
+		if(countDown==20&&lev<=3&&lives!=0&&velY!=0.7) {
+			String nextLev = "Next Level: " + new Integer(lev).toString() + ", ball speed will be faster! Get ready!";
+			g2d.drawString(nextLev, width/2-100, height / 20);
+		}
+		if(lev>3&&lives!=0) {
+			String Beat = "You have beaten the game! Your final score is: "+ new Integer(scoreBottom).toString();
+			g2d.drawString(Beat, width/2-100, height / 20);
+			String again = "Press left or right to start another game!";
+			g2d.drawString(again, width/2-100, height / 20+20);
+			scoreBottom = 0;
+		}
 
 	}
-	double velX=3*Math.cos(angle), velY=2.3;
-	@Override
+	double velX=3*Math.cos(angle), velY=0.7;
 	
 	public void actionPerformed(ActionEvent e) {
 		double g=0.0098;
 		velY+=g;
-		
 		//block1
 		if (ballY - blockY + 20 <= 3 && velY > 0 && ballY - blockY+20 >= 0) {
 			if (ballX + ballSize >= blockX && ballX <= blockX + 75) {
@@ -157,9 +218,62 @@ public class Game extends JPanel implements KeyListener, ActionListener {
 		}
 
 		if (ballY + ballSize > height) {
-			velY = -velY;
-
+			ballX = 20;
+			ballY = 20;
+			lives-=1;
+			t.stop();
+			t2.stop();
+			
+			if(lev==1) {
+				velX=3*Math.cos(angle);
+				velY=0.7;
+			}else if(lev==2) {
+				velX=3*Math.cos(angle)+0.2;
+				velY=0.7+0.2;
+			}else if(lev==3) {
+				velX=3*Math.cos(angle)+0.4;
+				velY=0.7+0.4;
+			}
 		}
+		
+		//beaten the game
+		if(lev>3) {
+			velX=3*Math.cos(angle);
+			velY=0.7;
+			ballX = 20;
+			ballY = 20;
+			lev=1;
+			lives = 3;
+			countDown=20;
+			t.stop();
+			t2.stop();
+			blockX = 100+rand.nextInt(300);
+			blockY = 200+rand.nextInt(50);
+			block2X = 100+rand.nextInt(200);
+			block2Y = 300+rand.nextInt(80);
+			block3X = 600+rand.nextInt(50);
+			block3Y = 200+rand.nextInt(100);
+		}
+		
+		if(countDown<=0) {
+			velX=3*Math.cos(angle);
+			velY=0.7;
+			ballX = 20;
+			ballY = 20;
+			lev+=1;
+			velX+=0.2; //increase speed for each level
+			velY+=0.2;
+			countDown=20;
+			t.stop();
+			t2.stop();
+			blockX = 100+rand.nextInt(300);
+			blockY = 200+rand.nextInt(50);
+			block2X = 100+rand.nextInt(200);
+			block2Y = 300+rand.nextInt(80);
+			block3X = 600+rand.nextInt(50);
+			block3Y = 200+rand.nextInt(100);
+		}
+		
 
 		// bottom pad
 		if (ballY + ballSize >= height - padH - inset && velY > 0) {
@@ -183,8 +297,6 @@ public class Game extends JPanel implements KeyListener, ActionListener {
 				bottomPadX += (bottomPadX < width - padW) ? SPEED : 0;
 			}
 		}
-
-		
 		time += 0.0000001;
 		repaint();
 
@@ -200,6 +312,8 @@ public class Game extends JPanel implements KeyListener, ActionListener {
 	@Override
 
 	public void keyPressed(KeyEvent e) {
+		t.start();
+		t2.start();
 		int code = e.getKeyCode();
 		switch (code) {
 		case KeyEvent.VK_LEFT:
@@ -213,8 +327,6 @@ public class Game extends JPanel implements KeyListener, ActionListener {
 		}
 
 	}
-
-
 
 	@Override
 
@@ -233,7 +345,7 @@ public class Game extends JPanel implements KeyListener, ActionListener {
 
 	}
 	public static void main(String[] args) {
-		JFrame test = new JFrame("test");
+		JFrame test = new JFrame("Game");
 		Game graph = new Game();
 		
 		test.add(graph);
